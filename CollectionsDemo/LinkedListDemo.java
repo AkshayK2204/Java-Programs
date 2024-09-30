33654081def7c8d470fd878242c27457f4604c6e
AkshayK2204 <akshay.kariyadan@people10.com>
2024-09-30 12:48:02 +0530

    Update LinkedListDemo.java

--- CollectionsDemo/LinkedListDemo.java
+++ CollectionsDemo/LinkedListDemo.java
@@ -15,16 +15,16 @@ public class LinkedListDemo {
         String first = list.getFirst();
         String last = list.getLast();
 
-        //String first1 = list.poll();
+        //String first2 = list.poll(); //poll deletes the first element and returns
         String first1 = list.peek();
         String last1 = list.peekLast();
 
         list.add(2,"Truck");
-        System.out.println(list.get(2));
+        //System.out.println(list.get(2));
         System.out.println(first + "\t" + last );
         System.out.println(first1 + "\t" + last1 );
         //peek() is safer since it returns null if list is empty, but getFirst() throws an exception
-
+        System.out.println(list.toString());
 
     }
 }
